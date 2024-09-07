df72f6055b875d4dcb15f2742236c9dd695acc2f
Karan Bhoi <karanbhoi3087@gmail.com>
2024-09-08 03:46:47 +0530

    latest product

--- src/app/Component/home/home.component.ts
+++ src/app/Component/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { SliderComponent } from '../slider/slider.component';
 import { ProductSectionComponent } from '../products-all-section/product-section.component';
 import { RouterLink } from '@angular/router';
+import { LatestproductComponent } from '../latestproduct/latestproduct.component';
+import { LoadingComponent } from '../loading/loading.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SliderComponent, ProductSectionComponent, RouterLink],
+  imports: [SliderComponent, LatestproductComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
