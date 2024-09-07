e983a0065c73a840d94a1101cdb843f10da7e8df
Karan Bhoi <karanbhoi3087@gmail.com>
2024-09-08 03:50:45 +0530

    user auth service

---
+++ src/app/Service/user-auth.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserAuthService {
+
+  setRoles(roles: []) {
+    if (typeof window !== 'undefined') { // Check if `window` is defined
+      localStorage.setItem("roles", JSON.stringify(roles));
+    } //because array of roles is not assign to the local storage so that' why we convet it to it to string
+  }
+
+  getRoles(): any[] {
+    const roles = localStorage.getItem('roles');
+    if (roles) {
+      return JSON.parse(roles)
+    }
+    return []
+  }
+
+  setBasicAuthString(basicauth: string) {
+    localStorage.setItem("basicAuth", basicauth)
+  }
+
+  getBasicAuthString() {
+    if (typeof window !== 'undefined') {
+      return localStorage.getItem("basicAuth")
+    }
+    return null;
+  }
+
+  clearLocalStorage() {
+    localStorage.clear()
+  }
+
+  isLoggedIn() {
+
+    if (this.getRoles() && this.getBasicAuthString()) {
+      return true
+    }
+    return false
+  }
+
+  private orderPlaced = new BehaviorSubject<boolean>(false);
+
+  constructor() { }
+
+  setOrderPlaced(status: boolean) {
+    this.orderPlaced.next(status);
+  }
+
+  isOrderPlaced(): boolean {
+    return this.orderPlaced.value;
+  }
+}
