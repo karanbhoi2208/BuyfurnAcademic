b43f793749be6ce144caea261ee5967a129d45b2
Karan Bhoi <karanbhoi3087@gmail.com>
2024-09-08 03:45:23 +0530

    cart

--- src/app/Component/cart/cart.component.ts
+++ src/app/Component/cart/cart.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../Service/product.service';
+import { response } from 'express';
+import { error } from 'console';
+import { NgFor, NgIf } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf, RouterLink],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+
+  products: any = [];
+  isNoProducts: boolean = false;
+  constructor(private productService: ProductService, private router: Router) { }
+  ngOnInit(): void {
+    this.getCartDetails();
+  }
+
+  getCartDetails() {
+    this.productService.getCartDetails().subscribe(
+      (response) => {
+        this.products = response;
+
+        if (this.products.length === 0) {
+          this.isNoProducts = true;
+        }
+      },
+      (error) => {
+        console.error('Error fetching cart details:', error);
+      }
+    );
+  }
+
+  checkOut() {
+
+    this.router.navigate(['/buyproduct'], {
+      queryParams: {
+        isSingleProductCheckout: false,
+        id: 0
+      }
+    })
+  }
+
+  removeProduct(id: any) {
+    this.productService.removeCartProduct(id).subscribe(
+      response => {
+        this.getCartDetails();
+        alert("Product removed");
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 
 }
