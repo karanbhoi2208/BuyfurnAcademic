7ff0b1f8bae03130222ee2a946a7785d81865c44
Karan Bhoi <karanbhoi3087@gmail.com>
2024-09-10 01:00:31 +0530

    add loading service

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,22 +1,30 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { LoadingService } from './Service/loading.service';
+import { finalize } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const loadingService = inject(LoadingService);
+
+  loadingService.showLoading();
 
   if (typeof localStorage !== 'undefined') {
-
     const authString = localStorage.getItem('basicAuth');
     if (authString) {
       const clonedRequest = req.clone({
         setHeaders: {
           Authorization: authString
         }
-
       });
-      console.log(authString);
+      // console.log(authString);
 
-      return next(clonedRequest);
+      return next(clonedRequest).pipe(
+        finalize(() => loadingService.hideLoading())
+      );
     }
   }
 
-  return next(req);
+  return next(req).pipe(
+    finalize(() => loadingService.hideLoading())
+  );
 };
