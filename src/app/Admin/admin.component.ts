65da612f7909866e97276035d877b512e76445aa
Karan Bhoi <karanbhoi3087@gmail.com>
2024-09-10 19:53:40 +0530

    improve navbar

--- src/app/Admin/admin.component.ts
+++ src/app/Admin/admin.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { AdminProfileComponent } from './admin-profile/admin-profile.component';
+import { SrvRecord } from 'dns';
+import { UserAuthService } from '../Service/user-auth.service';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, AdminProfileComponent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
 
+
+  constructor(private userAuthservice: UserAuthService) { }
+
+  user = this.userAuthservice.getUserName();
+
+  logOut() {
+    this.userAuthservice.clearLocalStorage()
+  }
 }
