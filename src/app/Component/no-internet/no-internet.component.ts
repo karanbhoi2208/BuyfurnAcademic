bd38927a13655ba625c28d947980f99bcbf30de7
Karan Bhoi <karanbhoi3087@gmail.com>
2024-09-12 13:19:38 +0530

    no internet

---
+++ src/app/Component/no-internet/no-internet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-internet',
+  standalone: true,
+  imports: [],
+  templateUrl: './no-internet.component.html',
+  styleUrl: './no-internet.component.css'
+})
+export class NoInternetComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoadingComponent } from './Component/loading/loading.component';
+import { NetworkStatusService } from './Service/network-status.service';
+import { NoInternetComponent } from './Component/no-internet/no-internet.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoadingComponent],
+  imports: [RouterOutlet, LoadingComponent, NoInternetComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FrontEnd';
+
+  isOnline = true;
+
+  constructor(private networkStatusService: NetworkStatusService) { }
+
+  ngOnInit() {
+    this.networkStatusService.isOnline.subscribe(status => {
+      this.isOnline = status;
+    });
+  }
 }
